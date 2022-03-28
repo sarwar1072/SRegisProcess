d6a1ef39cb88f4d853de1e8425086a9bcb61b4f3
sarwar <sarwar.cse.diu@gmail.com>
2022-03-28 16:58:13 +0600

    services

---
+++ RegistrationProcess/Framework/Services/IStudentRegistrationService.cs
@@ -0,0 +1,22 @@
+﻿using Framework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework.Services
+{
+   public interface IStudentRegistrationService
+    {
+        (IList<StudentRegistration> studentRegistrations, int total, int totalDisplay) GetStudentRegistrations(int pageindex,
+                                                                          int Pagesize,
+                                                                          string searchText,
+                                                                          string ordeBy);
+        void AddRegistration(StudentRegistration studentRegistration);
+        void EditRegistration(StudentRegistration studentRegistration);
+        StudentRegistration GetRegistration(int Id);
+        StudentRegistration DeleteRegistration(int Id);
+        IList<Student> GetStudents();
+        IList<Course> GetCourses();
+        int Count();
+    }
+}
---
+++ RegistrationProcess/Framework/Services/StudentRegistrationService.cs
@@ -0,0 +1,72 @@
+﻿using Framework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework.Services
+{
+   public class StudentRegistrationService : IStudentRegistrationService
+    {
+        private ISMUnitOfWork _sMUnitOfWork;
+        public StudentRegistrationService(ISMUnitOfWork sMUnitOfWork)
+        {
+            _sMUnitOfWork = sMUnitOfWork;
+        }
+
+        public void AddRegistration(StudentRegistration studentRegistration)
+        {
+            _sMUnitOfWork.StudentRegistrationRepository.Add(studentRegistration);
+            _sMUnitOfWork.Save();
+
+        }
+
+        public StudentRegistration DeleteRegistration(int Id)
+        {
+            var rgname = _sMUnitOfWork.StudentRegistrationRepository.GetById(Id);
+            _sMUnitOfWork.StudentRegistrationRepository.Remove(Id);
+            _sMUnitOfWork.Save();
+            return rgname;
+        }
+
+        public void Dispose()
+        {
+            _sMUnitOfWork.Dispose();
+        }
+
+        public void EditRegistration(StudentRegistration studentRegistration)
+        {
+            var editregis = _sMUnitOfWork.StudentRegistrationRepository.GetById(studentRegistration.Id);
+            editregis.StudentId = studentRegistration.StudentId;
+            editregis.CourseId = studentRegistration.CourseId;
+            editregis.EnrollDate = studentRegistration.EnrollDate;
+            editregis.IspaymentComplete = studentRegistration.IspaymentComplete;
+            _sMUnitOfWork.Save();
+        }
+
+        public StudentRegistration GetRegistration(int Id)
+        {
+            return _sMUnitOfWork.StudentRegistrationRepository.GetById(Id);
+        }
+
+        public (IList<StudentRegistration> studentRegistrations, int total, int totalDisplay) GetStudentRegistrations(int pageindex, int Pagesize, string searchText, string orderBy)
+        {
+            var result = _sMUnitOfWork.StudentRegistrationRepository.GetDynamic(null, orderBy, "Course,Student", pageindex, Pagesize, true);
+            return (result.data, result.total, result.totalDisplay);
+        }
+
+        public IList<Student> GetStudents()
+        {
+            return _sMUnitOfWork.StudentRepository.GetAll();
+        }
+
+        public IList<Course> GetCourses()
+        {
+            return _sMUnitOfWork.CourseRepository.GetAll();
+        }
+
+        public int Count()
+        {
+            return _sMUnitOfWork.StudentRegistrationRepository.GetCount();
+        }
+    }
+}
