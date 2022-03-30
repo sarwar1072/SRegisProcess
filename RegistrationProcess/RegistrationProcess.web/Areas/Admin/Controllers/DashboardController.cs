be62e2799863cc54d4ddbb26d0c59e6801ac60c9
sarwar <sarwar.cse.diu@gmail.com>
2022-03-30 18:09:46 +0600

    add admin section

---
+++ RegistrationProcess/RegistrationProcess.web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using Framework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistrationProcess.web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles= "SuperAdmin,Administrator")]
+    public class DashboardController : Controller
+    {
+        private SMDbContext _sMDbContext;
+        public DashboardController(SMDbContext sMDbContext)
+        {
+            _sMDbContext = sMDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
